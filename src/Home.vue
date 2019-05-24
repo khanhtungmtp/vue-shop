a3c43a3cdc60d6306e725db88089ffaf5fae46c2
khanhtungmtp <umlatao97@gmail.com>
2019-05-24 23:26:31 +0700

    Prepare Login Modal

--- src/Home.vue
+++ src/Home.vue
@@ -19,6 +19,7 @@
         </div>
       </div>
     </div>
+    <login></login>
   </div>
 </template>
 
@@ -26,11 +27,12 @@
 // @ is an alias to /src
 import Hero from '@/components/Hero.vue'
 import Products from '@/sections/Products.vue'
+import Login from '@/sections/Login.vue'
 
 export default {
   name: 'home',
   components: {
-    Hero, Products
+    Hero, Products, Login
   }
 }
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -20,7 +20,7 @@
         </ul>
         <form class="form-inline my-2 my-lg-0">
           <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <a class="btn btn-outline-success my-2 my-sm-0" data-toggle="modal" data-target="#login">Search</a>
         </form>
       </div>
     </nav>
