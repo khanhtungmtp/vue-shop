4f3f6359b50ab502ce982b150c9ea96696e5eb03
khanhtungmtp <umlatao97@gmail.com>
2019-05-25 12:48:28 +0700

    Firebase Integration

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyDEt5fvPnNllnwOdQd7JaWreS5l1pdqjE4',
+  authDomain: 'vue-shop-d76d4.firebaseapp.com',
+  databaseURL: 'https://vue-shop-d76d4.firebaseio.com',
+  projectId: 'vue-shop-d76d4',
+  storageBucket: 'vue-shop-d76d4.appspot.com',
+  messagingSenderId: '405600291689',
+  appId: '1:405600291689:web:1e353fdf3ee70bcd'
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
